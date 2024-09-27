3e6eb142b3f12532333b45a21295ffbfd61ce0ae
Jacob Gold <jacob@graphite.dev>
2024-09-26 23:07:05 -0700

    fix: more script fixes for null cases

--- latest_artifact.sh
+++ latest_artifact.sh
@@ -21,7 +21,10 @@ workflows=$(curl -sS \
 	https://api.github.com/repos/${repository}/actions/workflows)
 latest_workflow_id=$(echo ${workflows} \
 	| jq '.workflows[] | select(.name=="'${workflow_name}'").id')
-
+if [ "${latest_workflow_id}" == "null" ]; then
+  echoerr "No workflow found with name ${workflow_name}"
+  exit 0
+fi
 echoerr "Latest workflow ID: ${latest_workflow_id}"
 
 workflow_runs=$(curl -sS \
@@ -29,6 +32,10 @@ workflow_runs=$(curl -sS \
 		https://api.github.com/repos/${repository}/actions/workflows/${latest_workflow_id}/runs?status=success&branch=${branch_name})
 latest_workflow_run_id=$(echo ${workflow_runs} \
 		| jq '([.workflow_runs[] | select(.pull_requests | any(.number == '${pr_number}'))] | max_by(.run_number)) | .id')
+if [ "${latest_workflow_run_id}" == "null" ]; then
+  echoerr "No successful workflow run found for PR ${pr_number} on branch ${branch_name}"
+  exit 0
+fi
 echoerr "Latest workflow run ID: ${latest_workflow_run_id}"
 
 artifacts=$(curl -sS \
@@ -36,6 +43,10 @@ artifacts=$(curl -sS \
 	https://api.github.com/repos/${repository}/actions/runs/${latest_workflow_run_id}/artifacts)
 latest_artifact_id=$(echo ${artifacts} \
 	| jq '.artifacts[] | select(.name=="'${artifact_name}'").id')
+if [ "${latest_artifact_id}" == "null" ]; then
+  echoerr "No artifacts found for workflow run ${latest_workflow_run_id}"
+  exit 0
+fi
 echoerr "Latest artifact ID: ${latest_artifact_id}"
 
 curl -sSL -H "Accept: application/vnd.github+json" \
